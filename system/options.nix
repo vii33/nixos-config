ca21ba483aebe1b41178560be3ca8d46c63637c4
vii <repo.specific.email@example.com>
2025-09-10 14:45:43 +0200

    intial config

---
+++ system/options.nix
@@ -0,0 +1,8 @@
+# /etc/nixos/system/options.nix
+{ config, lib, pkgs, ... }:
+
+{
+  # This file is for defining custom NixOS options or setting
+  # miscellaneous options for your system.
+  # It is currently empty but correctly formatted.
+}
