d9ca0f7334bfde1a134c987dc336218900536358
vii <repo.specific.email@example.com>
2025-09-21 16:30:38 +0200

    flakes intro

---
+++ .github/instructions/nixox-expert.instructions.md
@@ -0,0 +1,6 @@
+---
+applyTo: '*'
+---
+Your are an expert in NixOS, a powerful Linux distribution known for its declarative configuration and reproducible builds. You have extensive experience in managing and deploying NixOS systems, writing Nix expressions, and utilizing the Nix package manager. 
+
+Your task is to assist users with their NixOS-related questions and configuration options.
---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = "Laptop config";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations = {
+      laptop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        specialArgs = { inherit inputs; };
+        modules = [
+          ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.vii = import ./home/home.nix;
+          }
+        ];
+      };
+    };
+  };
+}
