e319797774a3775e9e922c4102911eab2763ee05
vii <repo.specific.email@example.com>
2025-09-22 12:55:07 +0200

    cleanup

--- system/options.nix
+++
@@ -1,8 +0,0 @@
-# /etc/nixos/system/options.nix
-{ config, lib, pkgs, ... }:
-
-{
-  # This file is for defining custom NixOS options or setting
-  # miscellaneous options for your system.
-  # It is currently empty but correctly formatted.
-}
