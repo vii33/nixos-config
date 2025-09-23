926a34699bf4f0fa5f997df1d00549ef2c052568
vii <repo.specific.email@example.com>
2025-09-23 21:55:41 +0200

    working swap for laptop

--- hosts/laptop/swap.nix
+++ hosts/laptop/swap.nix
@@ -1,18 +1,27 @@
 {
-  # On-disk swap big enough for hibernation + 25% extra
+  # On-disk swap big enough for hibernation + 25% extra to avoid out of memory issues
+  # swap file first needs to be create manually
+  # then offset needs to be set with `filefrag -v /swapfile | awk '{ if($1=="0:"){print $4} }'`
+  # then hibernate needs to be used once with `sudo systemctl hibernate` -- after this it should
+  # be visible in start menu
   swapDevices = [
-    { device = "/swapfile"; size = 12288; }  # 12 GB swapfile
+    { 
+      device = "/swapfile"; 
+      size = 12*1024; 
+      priority = 100;
+    }  
   ];
 
   # Tell systemd+kernel where to resume, UUID: *underlying partition* that holds the swapfile
   boot.resumeDevice = "/dev/disk/by-uuid/89a00461-4909-46da-ae07-1e17e5032e2b";
+  boot.kernelParams = ["resume_offset=18022400" ];  # change offset to the one you got from filefrag
 
   # Compressed RAM for compensating spikes in memory usage
   zramSwap = {
     enable = true;
-    memoryPercent = 20;   # ~2 GB fast compressed swap
+    memoryPercent = 15;   # ~1 GB fast compressed swap
     algorithm = "zstd";
-    priority = 100;
+    priority = 10;
   };
 
   boot.kernel.sysctl."vm.swappiness" = 20;  # default is 60, lower means less swap
