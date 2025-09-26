29ebff51f081294e389fbc0079e89cd60a882eb5
vii33 <1887246+vii33@users.noreply.github.com>
2025-09-26 13:59:08 +0200

    setup fish shortcuts

--- modules/home-manager/fish-shell.nix
+++ modules/home-manager/fish-shell.nix
@@ -9,5 +9,21 @@
     set -U sponge_delay 5
   '';
 
+  # Fish key bindings via Home Manager
+  # Find out key codes with `fish_key_reader`
+  programs.fish = {
+    enable = true;
+    functions.fish_user_key_bindings.body = ''
+      # CTRL + U    whole line delete
+      bind ctrl-u kill-whole-line
+      bind -M insert ctrl--u kill-whole-line
 
+      # CTRL + L   clear screen like bash
+      bind \cl clear-screen
+
+      # Ctrl+Right / Ctrl+Left for word-wise movement
+      bind ctrl-right forward-word
+      bind ctrl-left  backward-word
+    '';
+  };
 }
