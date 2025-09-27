ebdadbdb501c5f3902da13199e6efb87d4181d65
vii33 <1887246+vii33@users.noreply.github.com>
2025-09-27 17:54:47 +0200

    enabled command not found package

--- profiles/system/common.nix
+++ profiles/system/common.nix
@@ -4,6 +4,9 @@
   # Enable fish shell system-wide
   programs.fish.enable = true;
 
+  # Enable command-not-found to suggest packages for missing commands
+  programs.command-not-found.enable = true;
+
   environment.systemPackages = with pkgs; [
     nfs-utils     # NFS share
     vim
