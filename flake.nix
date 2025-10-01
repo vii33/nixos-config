277bda0fa8dcad5ea720c71314f2f34b93eedfa3
vii <repo.specific.email@example.com>
2025-10-01 20:56:47 +0200

    first draft nixvim

--- flake.nix
+++ flake.nix
@@ -7,28 +7,33 @@
         url = "github:nix-community/home-manager/release-25.05";
         inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
   };
 
-  outputs = { self, nixpkgs, home-manager, ... }@inputs:   # make specific inputs (self, nixpkgs..) from the inputs attribut set available, addtionally put the entire attribute set in the inputs variable
+  outputs = { self, nixpkgs, home-manager, nixvim, ... }@inputs:   # make specific inputs (self, nixpkgs..) from the inputs attribut set available, addtionally put the entire attribute set in the inputs variable
   {
     nixosConfigurations = {
 
       laptop = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };        # make inputs available in (sub)modules
-        modules = [ ./hosts/laptop/composer.nix ];
+        modules = [ ./hosts/laptop/default.nix ];
       };
 
       home-server = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         specialArgs = { inherit inputs; };       # make inputs available in (sub)modules
-        modules = [ ./hosts/home-server/composer.nix ];
+        modules = [ ./hosts/home-server/default.nix ];
       };
 
       work = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";  # WSL environment (headless dev)
         specialArgs = { inherit inputs; };
-        modules = [ ./hosts/work/composer.nix ];
+        modules = [ ./hosts/work/default.nix ];
       };
     };
   };
