baea348e1ef99fd802c296c3ecdc7f41d904df54
vii <repo.specific.email@example.com>
2025-11-15 21:10:10 +0100

    switched to latest vscode in unstable branch

--- profiles/home/development-desktop.nix
+++ profiles/home/development-desktop.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, pkgs-unstable, ... }:
 
 {
   imports = [
@@ -6,6 +6,6 @@
   ];
 
   home.packages = with pkgs; [
-    vscode
+    pkgs-unstable.vscode
   ];
 }
