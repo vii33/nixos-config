de557dc7328e481dd8c640a1b9b9e23e7df9d16d
vii33 <1887246+vii33@users.noreply.github.com>
2025-11-21 22:36:07 +0100

    cleanup

--- profiles/home/development-headless.nix
+++ profiles/home/development-headless.nix
@@ -3,10 +3,7 @@
 {
   imports = [
     ../../modules/home/fish-shell.nix
-    #../../modules/home/neovim.nix
-    #../../modules/home/nixvim/default.nix
     ../../modules/home/nixvim/lazyvim.nix
-    #../../modules/home/nixvim/relief.nix
   ];
 
   home.packages = with pkgs; [
--- profiles/system/desktop.nix
+++ profiles/system/desktop.nix
@@ -1,12 +1,6 @@
 { config, pkgs, ... }:
 
 {
-  # environment.systemPackages = with pkgs; [
-  #   # Add packages here
-  # ];
-
-  programs.firefox.enable = true;
-
   fonts.packages = with pkgs; [
     nerd-fonts.meslo-lg          # Used by Fish Shell / Alacritty
     nerd-fonts.jetbrains-mono
