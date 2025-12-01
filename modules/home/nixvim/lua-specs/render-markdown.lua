df7afe1cfcf4fde84ce8087c39cd0372af22266d
vii33 <1887246+vii33@users.noreply.github.com>
2025-12-01 21:24:12 +0100

    tried to fix markdown renderer nvim

--- modules/home/nixvim/lua-specs/render-markdown.lua
+++ modules/home/nixvim/lua-specs/render-markdown.lua
@@ -1,11 +1,14 @@
 -- Disable LaTeX rendering to avoid tree-sitter parser warnings
+-- Also ensure proper lazy loading to prevent setup() errors in snacks explorer
 return {
   {
     "MeanderingProgrammer/render-markdown.nvim",
+    ft = { "markdown", "norg", "rmd", "org", "codecompanion" },
     opts = {
       latex = {
         enabled = false,
       },
+      file_types = { "markdown", "norg", "rmd", "org", "codecompanion" },
     },
   },
 }
