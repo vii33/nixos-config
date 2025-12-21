5da5f8b0e96878e680f2cb7f3dd87599848fa55f
vii33 <1887246+vii33@users.noreply.github.com>
2025-12-21 21:26:02 +0100

    defined now work profile

---
+++ profiles/home/work.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, pkgs-unstable, ... }:
+
+{
+  imports = [
+    ../../modules/home/fish-shell.nix
+    ../../modules/home/nixvim/lazyvim.nix
+  ];
+
+  home.packages = with pkgs; [
+    #pkgs-unstable.opencode
+    #pkgs-unstable.github-copilot-cli
+  ];
+  
+
+}
