d5e8b82663b0d3701c55ee7e8b5b37399576b96e
vii33 <1887246+vii33@users.noreply.github.com>
2026-01-01 22:04:22 +0100

    updated brew apps descriptions

--- hosts/work/brew.nix
+++ hosts/work/brew.nix
@@ -5,14 +5,13 @@
     enable = true;
     
     onActivation = {
-      #cleanup = "zap"; # Deletes brew packages that are no longer in this list
+      #cleanup = "zap";   # Uninstalls brew packages that are no longer in this list
       autoUpdate = true;
       upgrade = true;
     };
 
-    # Brew: terminal tools and packages
+    # Brew: terminal tools and packages. Check for further brew options: https://nix-darwin.github.io/nix-darwin/manual/#opt-homebrew.brews
     brews = [
-      #"mas" # CLI Tool für den Mac App Store (wichtig für masApps)
       "neovim"
       #"pnpm"
       "docker-compose"    # Check installation instructions: https://formulae.brew.sh/formula/docker-compose#default
@@ -20,24 +19,26 @@
 
     # Cask: macOS GUI applications
     casks = [
+
       # UTILS ###################################################
-      #"visual-studio-code"
-      #"obsidian"
-      #"bitwarden"
+      #"visual-studio-code"  # TODO: Check if auto-updates work with brew version
+      #"obsidian"         # TODO: Check if auto-updates work with brew version
+      #"bitwarden"        # TODO: Check if auto-updates work with brew version
       #"spotify"
       "drawio"
       "rectangle"         # Window Management
       #"tiles"            # Window management (alternative to Rectangle)
       "soundanchor"       # Pin sound output
-      "qview"             # Quick image viewer
-      "karabiner-elements"# Keyboard remapping
-      "tuist"             # Keyboard Cowboy
+      #"karabiner-elements"# Keyboard remapping
       "shortcat"          # Control macOS with the keyboard
       "rocket"            # Emoji picker
-      "mos"               # Smooth mouse scrolling
       #"battery"
       #"vanilla"          # Menu bar items manager, drains battery (especially bluetooth, use few items)
-
+      "nikitabobko/tap/aerospace"  # Tiling window manager // Alternative: yabai + skhd
+      "leader-key"        # vim like keybindings - https://github.com/mikker/LeaderKey
+      "raycast"           # Spotlight replacement // Alternative: alfred
+      "macs-fan-control"
+      
       # DEV #####################################################
       "kitty"             # Terminal emulator
       "insomnia"
@@ -45,6 +46,7 @@
       "syntax-highlight"  # Syntax highlighting for macOS Quick Look. Needs to be opened once manually after install to set up.
       "qlmarkdown"        # Markdown preview in macOS Quick Look. Needs to be opened once manually after install to set up.
       "cyberduck"         # FTP/S3 client
+      "keyclu"          # Show shortcuts for current app
     ];
   };
-}
+}  
