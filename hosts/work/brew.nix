9a2c028d79724c8fb8f1fbf2459028cb78a45240
vii33 <1887246+vii33@users.noreply.github.com>
2026-01-15 13:56:17 +0100

    updated brew packages

--- hosts/work/brew.nix
+++ hosts/work/brew.nix
@@ -18,7 +18,6 @@
     ];
 
     # Cask: macOS GUI applications
-    casks = [
 
       # UTILS ###################################################
       #"visual-studio-code"  # TODO: Check if auto-updates work with brew version
@@ -31,14 +30,15 @@
       "soundanchor"       # Pin sound output
       #"karabiner-elements"# Keyboard remapping
       "shortcat"          # Control macOS with the keyboard
-      #"rocket"            # Emoji picker  // Not needed as done with leader-key and raycast
+      #"rocket"           # Emoji picker  // Not needed as done with leader-key and raycast
       "aldente"           # Optimize battery health (80% charge limit) - Alternative: battery (Does not work well with managed MacOS)
-      #"hidden"           # Hide menu bar items // Alternative: vanilla (needs screen recording permission .-/)
-      "nikitabobko/tap/aerospace"  # Tiling window manager // Alternative: paneru (via nixos!)
+      "jordanbaird-ice"   # Hide menu bar items // Alternative: vanilla (needs screen recording permission .-/)
+      "nikitabobko/tap/aerospace"  # Tiling window manager // Alternative: paneru (via nixos!812320)
       "leader-key"        # vim like keybindings - https://github.com/mikker/LeaderKey
       "raycast"           # Spotlight replacement // Alternative: alfred
       "macs-fan-control"  # Fan conrol for Mac Notebooks
       "mac-mouse-fix"     # Mouse gestures
+      #"linearmouse"    
       
       # DEV #####################################################
       "kitty"             # Terminal emulator
