1288e66b438ff2b70f450ed7ff03805adf0d5ec9
vii33 <1887246+vii33@users.noreply.github.com>
2026-06-12 12:29:22 +0200

    Add fish keep-awake helper

--- modules/home/fish-keep-awake-test.nix
+++ modules/home/fish-keep-awake-test.nix
@@ -1,6 +1,30 @@
-# test
+# modules/home/fish-keep-awake.nix
 { pkgs, lib, ... }:
 
 lib.mkIf pkgs.stdenv.isLinux {
-  programs.fish.shellAbbrs.ka = "keepawake";
+  programs.fish = {
+    shellAbbrs = {
+      ka = "keepawake";
+    };
+
+    functions.keepawake = {
+      description = "Keep the laptop awake for N minutes";
+      body = ''
+        set -l minutes $argv[1]
+
+        if test -z "$minutes"
+          set minutes 120
+        end
+
+        if not string match -qr '^[1-9][0-9]*$' -- "$minutes"
+          echo "Usage: keepawake <minutes>"
+          echo "Example: keepawake 120"
+          return 2
+        end
+
+        echo "Keeping system awake for $minutes minutes. Press Ctrl+C to cancel."
+        command systemd-inhibit --what=sleep:idle --why="keepawake $minutes minutes" -- sleep "$minutes"m
+      '';
+    };
+  };
 }
